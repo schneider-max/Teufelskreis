5e7983c3c9d2df857c240438cd17063f62c760de
schneider-max <schneider_max@gmx.at>
2021-09-27 21:16:45 +0200

    JS Datei eingefügt

---
+++ src/js/app.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let tmpCard = document.getElementById("Karten");
+
+    tmpCard.addEventListener("click", function(){
+
+        // Value vom Sourcen Ordner zurgreifen
+
+        tmpCard.src = tmpCard.src.replace
+    });
+});
